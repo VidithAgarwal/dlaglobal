521c34dc479b7cebc275cfcfb22e59e63c714f74
Vidith <vidithagarwal315>
2021-05-17 22:44:07 +0530

    Prepare for deployment

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,4 @@
-import User from '../models/UserModel.js';
+import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateJsonwebtoken.js';
 
